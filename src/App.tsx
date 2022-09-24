165cb6d81df2bdecf3d3e21a83fd798432b6db19
Masaru Hori <qirex_rr@yahoo.co.jp>
2022-09-24 17:08:38 +0900

    盤面を追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,8 @@ import { createField, CellData, FieldData, addCandidate, removeCandidate } from 
 import './App.css';
 
 function App() {
-  const field: FieldData = createField([[0, 1, 2, 3, 5], [10, 15, 20, 21, 22], [16, 17, 18, 23, 24], [4, 9, 13, 14, 19], [6, 7, 8, 11, 12]]);
+  const field: FieldData = createField()
   const [commandNo, setCommandNo] = React.useState<number>(-1)
-
-  /*
-  for (let i = 0; i < 25; i++) {
-    field.cells[i].value = i + 1
-  }
-  */
 
   const toggleValue = (cell: CellData, value: number): CellData => {
     if (cell.value && cell.value === value) {
@@ -32,6 +26,17 @@ function App() {
       return addCandidate(cell, value)
     }
   }
+  const toggleColor = (cell: CellData, color: string): CellData => {
+    if (cell.color) {
+      if (cell.color === color) {
+        return { ...cell, color: undefined }
+      } else {
+        return { ...cell, color: color}
+      }
+    } else {
+      return { ...cell, color: color}
+    }
+  }
 
   const commandStyleValue: { [key: string]: string } = { fontSize: "48px" }
   const commandStyle: { [key: string]: string } = { color: "white", fontSize: "24px", textAlign: "left" }
@@ -46,11 +51,11 @@ function App() {
     { contents: "3", style: { ...commandStyle, paddingLeft: "45%" }, func: (cell: CellData) => { return toggleCandidate(cell, 3) } },
     { contents: "4", style: { ...commandStyle, paddingLeft: "67%" }, func: (cell: CellData) => { return toggleCandidate(cell, 4) } },
     { contents: "5", style: { ...commandStyle, paddingLeft: "80%" }, func: (cell: CellData) => { return toggleCandidate(cell, 5) } },
-    { contents: "", style: { background: "lightpink" }, func: (cell: CellData) => { return { ...cell, color: "lightpink" } } },
-    { contents: "", style: { background: "mediumorchid" }, func: (cell: CellData) => { return { ...cell, color: "mediumorchid" } } },
-    { contents: "", style: { background: "slateblue" }, func: (cell: CellData) => { return { ...cell, color: "slateblue" } } },
-    { contents: "", style: { background: "darksalmon" }, func: (cell: CellData) => { return { ...cell, color: "darksalmon" } } },
-    { contents: "", style: { background: "lightgreen" }, func: (cell: CellData) => { return { ...cell, value: undefined, candidate: undefined, color: undefined } } },
+    { contents: "", style: { background: "lightpink" }, func: (cell: CellData) => { return toggleColor(cell, "lightpink") } },
+    { contents: "", style: { background: "mediumorchid" }, func: (cell: CellData) => { return toggleColor(cell, "mediumorchid") } },
+    { contents: "", style: { background: "slateblue" }, func: (cell: CellData) => { return toggleColor(cell, "slateblue") } },
+    { contents: "", style: { background: "darksalmon" }, func: (cell: CellData) => { return toggleColor(cell, "darksalmon") } },
+    { contents: "CLEAR", style: { fontSize: "24px", background: "lightgreen" }, func: (cell: CellData) => { return { ...cell, value: undefined, candidate: undefined, color: undefined } } },
   ]
 
   return (<>
--- src/__test__/game.test.ts
+++ src/__test__/game.test.ts
@@ -11,18 +11,19 @@ import {
 } from "../game";
 
 test("Fieldが生成される", () => {
-  const field: FieldData = createField([]);
+  const field: FieldData = createField();
   expect(field.cells.length).toBe(25);
 });
 
 test("周囲が壁のField生成される", () => {
-  const field: FieldData = createField([]);
+  const field: FieldData = createField();
   expect(field.cells[0].wall).toBe(WALL.TOP | WALL.LEFT);
   expect(field.cells[YOKO - 1].wall).toBe(WALL.TOP | WALL.RIGHT);
   expect(field.cells[(TATE - 1) * YOKO].wall).toBe(WALL.LEFT | WALL.BOTTOM);
   expect(field.cells[TATE * YOKO - 1].wall).toBe(WALL.RIGHT | WALL.BOTTOM);
 });
 
+/*
 test("BLOCKの壁を持つField生成される", () => {
   const field: FieldData = createField([[0, 1, 2, 3, 5]]);
   expect(field.cells[0].wall).toBe(WALL.TOP | WALL.LEFT);
@@ -31,6 +32,7 @@ test("BLOCKの壁を持つField生成される", () => {
   expect(field.cells[3].wall).toBe(WALL.TOP | WALL.BOTTOM | WALL.RIGHT);
   expect(field.cells[5].wall).toBe(WALL.LEFT | WALL.BOTTOM | WALL.RIGHT);
 });
+*/
 
 test("setValueはvalueを指定の値に設定する", () => {
   const cell: CellData = { wall: WALL.NONE };
