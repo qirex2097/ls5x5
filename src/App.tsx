eda770412d4df4da85534e06b4691956b5739bf7
Masaru Hori <qirex_rr@yahoo.co.jp>
2022-09-29 10:02:00 +0900

    見た目

--- src/App.tsx
+++ src/App.tsx
@@ -97,20 +97,21 @@ function App() {
     { contents: "", style: { background: "mediumorchid" }, func: (cell: CellData) => { return toggleColor(cell, "mediumorchid") } },
     { contents: "", style: { background: "slateblue" }, func: (cell: CellData) => { return toggleColor(cell, "slateblue") } },
     { contents: "", style: { background: "darksalmon" }, func: (cell: CellData) => { return toggleColor(cell, "darksalmon") } },
-    { contents: "CLEAR", style: { fontSize: "24px", background: "lightgreen" }, func: (cell: CellData) => { return { ...cell, value: undefined, candidate: undefined, color: undefined } } },
+    { contents: "", style: { fontSize: "24px", background: "lightgreen" }, func: (cell: CellData) => { return { ...cell, value: undefined, candidate: undefined, color: undefined } } },
   ]
 
   type BoardCommand = CommandLooks & {
     func: () => void
   }
 
-  const commandStyleValue: { [key: string]: string } = { fontSize: "48px" }
+  //  const commandStyleValue: { [key: string]: string } = { fontSize: "48px" }
+  const background: string = "khaki"
   const commands2: BoardCommand[] = [
-    { contents: "1", style: commandStyleValue, func: () => { } },
-    { contents: "1", style: commandStyleValue, func: () => { } },
-    { contents: "1", style: commandStyleValue, func: () => { } },
-    { contents: "1", style: commandStyleValue, func: () => { } },
-    { contents: "OK", style: { background: "darkgrey", fontSize: "24px" }, func: () => { console.log(`pushed OK`) } },
+    { contents: "↪️", style: { background: "white", fontSize: "64px" }, func: () => { } },
+    { contents: "↩️", style: { background: "white", fontSize: "64px" }, func: () => { } },
+    { contents: "", style: { background: background, border: "none" }, func: () => { } },
+    { contents: "", style: { background: background, border: "none" }, func: () => { } },
+    { contents: "✔️", style: { background: "white", fontSize: "64px" }, func: () => { console.log(`pushed OK`) } },
   ]
 
   const handleCellClick = (cell: CellData): CellData => {
@@ -118,12 +119,12 @@ function App() {
     return commands[commandNo].func(cell)
   }
 
-  return (<>
+  return (<div style={{margin: 0, padding: "20px", background: background, position: "relative"}}>
     <Field cells={field.cells} func={handleCellClick} />
     <Commands commands={commands} commandNo={commandNo}
       selectCommand={(newCommandNo: number) => { if (newCommandNo === commandNo) setCommandNo(-1); else setCommandNo(newCommandNo) }} />
     <Commands commands={commands2} selectCommand={(i: number) => { commands2[i].func() }} />
-  </>);
+  </div>);
 }
 
 export default App;
